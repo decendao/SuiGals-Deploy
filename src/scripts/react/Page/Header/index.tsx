397888c849dc64a7c1a727be480a3ad76c4cd689
fange123 <56150056+fange123@users.noreply.github.com>
2023-04-02 15:36:13 +0800

    feat:添加首页底部链接

--- src/scripts/react/Page/Header/index.tsx
+++ src/scripts/react/Page/Header/index.tsx
@@ -1,72 +1,91 @@
-import "./index.scss"
-import {useEffect} from "react";
-import $ from "jquery"
+import "./index.scss";
+import { useEffect } from "react";
+import $ from "jquery";
 type Props = {
-  show(): void
-  connect():void
-  noConnect: boolean
-  userAddress: string|null
-}
+  show(): void;
+  connect(): void;
+  noConnect: boolean;
+  userAddress: string | null;
+};
 export default function Header(props: Props) {
   function id(str: string | null) {
-    return str ? str.slice(0, 4) + "..." + str.slice(-4) : ""
+    return str ? str.slice(0, 4) + "..." + str.slice(-4) : "";
   }
   useEffect(() => {
-    $(".menu-btn").click(function() {
-      $(".top-nav").addClass("active")
-    })
+    $(".menu-btn").click(function () {
+      $(".top-nav").addClass("active");
+    });
 
     $("#header .m-d, #header .close").click(() => {
-      $(".top-nav").removeClass("active")
-    })
-  }, [])
+      $(".top-nav").removeClass("active");
+    });
+  }, []);
   return (
-    <div id="header">
-      <div className="top-nav">
-        <img className="logo" src="/img/logo.png" alt=""/>
-        <div className="m-d"/>
-        <div className="right">
-          <img src="/img/close.png" className="close"/>
-          <div className="nav-list">
-            <a className="nav-item" href="">
+    <div id='header'>
+      <div className='top-nav'>
+        <img className='logo' src='/img/logo.png' alt='' />
+        <div className='m-d' />
+        <div className='right'>
+          <img src='/img/close.png' className='close' />
+          <div className='nav-list'>
+            <a className='nav-item' href=''>
               HOME
             </a>
-            <a className="nav-item" href="#about">
+            <a className='nav-item' href='#about'>
               NFT
             </a>
-            <a className="nav-item" href="#roadmap">
+            <a className='nav-item' href='#roadmap'>
               ROADMAP
             </a>
-            <a className="nav-item" href="#team">
+            <a className='nav-item' href='#team'>
               TEAM
             </a>
-            <a className="nav-item" href="#Concept">
+            <a className='nav-item' href='#Concept'>
               ART
             </a>
-            <a className="nav-item" href="#faq">
+            <a className='nav-item' href='#faq'>
               FAQ
             </a>
           </div>
 
-          <div className="icon-list">
-            <a className="icon-item" href="#" target="_blank">
-              <img src="/img/twitter_1.png" alt=""/>
+          <div className='icon-list'>
+            <a className='icon-item' href='#' target='_blank'>
+              <img src='/img/twitter_1.png' alt='' />
             </a>
-            <a className="icon-item" href="#" target="_blank">
-              <img src="/img/discord_1.png" alt=""/>
+            <a className='icon-item' href='#' target='_blank'>
+              <img src='/img/discord_1.png' alt='' />
             </a>
-            <a className="icon-item" href="#" target="_blank">
-              <img src="/img/opensea_1.png" alt=""/>
+            <a className='icon-item' href='#' target='_blank'>
+              <img src='/img/opensea_1.png' alt='' />
             </a>
           </div>
         </div>
-        <div className="menu-btn">
-          <img src="/img/menu.png" alt=""/>
+        <div className='menu-btn'>
+          <img src='/img/menu.png' alt='' />
         </div>
       </div>
-      <video id="header-video" src="/video/headervid1.mp4" autoPlay loop muted/>
-
-   
+      <video
+        id='header-video'
+        src='/video/headervid1.mp4'
+        autoPlay
+        loop
+        muted
+      />
+      {/* home页底部链接 */}
+      <div className='home_foot'>
+        <div>
+          <a href=''></a>
+        </div>
+        <div>
+          <a href=''></a>
+        </div>
+        <div>
+          <a href=''></a>
+        </div>
+        <div>
+          <a href=''></a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
