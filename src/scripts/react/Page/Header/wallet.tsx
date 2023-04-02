55a6d82a8c4eec20be6983f7ffb5a669e3cb49f9
fange123 <56150056+fange123@users.noreply.github.com>
2023-04-02 14:59:31 +0800

    no message

--- src/scripts/react/Page/Header/wallet.tsx
+++ src/scripts/react/Page/Header/wallet.tsx
@@ -1,24 +1,28 @@
 {
-    props.noConnect ? (
-      <div className="connect" onClick={()=>{
-        props.connect()
-      }}>
-        <img src="/img/wallet.png" alt=""/>
-        CONNECT WALLET
-      </div>
-    ) : (
-      <div className="connect" onClick={()=>{
-        console.log("show")
-        props.show()
-      }}>
-        <img src="/img/wallet.png" alt=""/>
-        {id(props.userAddress)}
-      </div>
-    )
-  }
+  props.noConnect ? (
+    <div
+      className='connect'
+      onClick={() => {
+        props.connect();
+      }}
+    >
+      <img src='/img/wallet.png' alt='' />
+      CONNECT WALLET
+    </div>
+  ) : (
+    <div
+      className='connect'
+      onClick={() => {
+        console.log("show");
+        props.show();
+      }}
+    >
+      <img src='/img/wallet.png' alt='' />
+      {id(props.userAddress)}
+    </div>
+  );
+}
 
-
-
-  <div className="coming-button" onClick={props.show}>
-      Connect
-      </div>
+<div className='coming-button' onClick={props.show}>
+  Connect
+</div>;
